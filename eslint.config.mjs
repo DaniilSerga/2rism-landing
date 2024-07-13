2d5e9d08c25f80cca4bd525a3ecf53dbc5ec1798
DaniilSerga <daniil.serga@gmail.com>
2024-07-14 01:23:00 +0300

    feat: set up eslint

---
+++ eslint.config.mjs
@@ -0,0 +1,57 @@
+import path from "node:path";
+import {fileURLToPath} from "node:url";
+
+import {fixupConfigRules, fixupPluginRules} from "@eslint/compat";
+import {FlatCompat} from "@eslint/eslintrc";
+import js from "@eslint/js";
+import typescriptEslint from "@typescript-eslint/eslint-plugin";
+import typescriptParser from "@typescript-eslint/parser";
+import prettier from "eslint-plugin-prettier";
+import react from "eslint-plugin-react";
+import simpleImportSort from "eslint-plugin-simple-import-sort";
+import globals from "globals";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const compat = new FlatCompat({
+    baseDirectory: __dirname,
+    recommendedConfig: js.configs.recommended,
+    allConfig: js.configs.all,
+});
+
+export default [
+    ...fixupConfigRules(
+        compat.extends(
+            "eslint:recommended",
+            "plugin:react/recommended",
+            "plugin:@typescript-eslint/recommended",
+            "prettier",
+        ),
+    ),
+    {
+        plugins: {
+            react: fixupPluginRules(react),
+            "@typescript-eslint": fixupPluginRules(typescriptEslint),
+            prettier,
+            "simple-import-sort": simpleImportSort,
+        },
+        languageOptions: {
+            ecmaVersion: "latest",
+            sourceType: "module",
+            parser: typescriptParser,
+            parserOptions: {
+                ecmaFeatures: {
+                    jsx: true,
+                },
+            },
+            globals: {
+                ...globals.browser,
+            },
+        },
+        rules: {
+            "prettier/prettier": "error",
+            "simple-import-sort/imports": "error",
+            "simple-import-sort/exports": "error",
+        },
+    },
+];
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,8 +1,21 @@
+import logo from "assets/icons/logo.svg";
 import React, {FC} from "react";
+
 import styles from "./Header.module.scss";
 
 const Header: FC = () => {
-    return <header></header>;
+    return (
+        <header className={styles.header}>
+            {/* logo */}
+            <div>
+                <img src={logo} alt="" />
+            </div>
+            {/* navbar */}
+            <nav></nav>
+            {/* avatar */}
+            <div></div>
+        </header>
+    );
 };
 
 export default Header;
